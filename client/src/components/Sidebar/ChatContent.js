0521a612153e527cf7b575c0f9087dd3dfca705d
Ella N <38847123+ellanan@users.noreply.github.com>
2022-02-14 15:51:22 -0500

    show + styling unread messages count/messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -9,6 +9,9 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: 20,
     flexGrow: 1,
   },
+  container: {
+    width: "100%",
+  },
   username: {
     fontWeight: "bold",
     letterSpacing: -0.2,
@@ -18,6 +21,26 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  previewUnreadText: {
+    fontSize: 12,
+    letterSpacing: -0.17,
+    color: "#000000",
+    fontWeight: "bold",
+  },
+  unreadMessagesWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    },
+  unreadMessagesCount: {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: "#FFF",
+    backgroundColor: "#6091FC",
+    padding: "0.2rem 0.5rem",  
+    borderRadius: "50px",
+    marginRight: "10px",
+    },
 }));
 
 const ChatContent = (props) => {
@@ -25,16 +48,24 @@ const ChatContent = (props) => {
 
   const { conversation } = props;
   const { latestMessageText, otherUser } = conversation;
+  const unreadMessages = conversation.messages.filter((message) => message.senderId === otherUser.id && !message.readByRecipient ).length;
 
   return (
     <Box className={classes.root}>
-      <Box>
+      <Box className={classes.container}>
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Box className={classes.unreadMessagesWrapper}>
+         <Typography className={unreadMessages > 0 ? classes.previewUnreadText: classes.previewText}>
           {latestMessageText}
         </Typography>
+        {unreadMessages > 0 && (
+          <Typography className={classes.unreadMessagesCount}>
+            {unreadMessages}
+          </Typography>
+        )}
+        </Box>
       </Box>
     </Box>
   );
