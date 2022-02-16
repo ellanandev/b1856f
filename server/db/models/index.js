c0d09f5cff1579c2338cba922f2572f169cb53f8
ellanan <38847123+ellanan@users.noreply.github.com>
2022-02-15 22:22:17 -0500

    redefine relationship between Conversation and User
    this relationship now goes through ConversationParticipant, to support more than 2 users per conversation
    
    the old relationship that was assuming two users per conversation is removed

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,12 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const ConversationParticipant = require("./conversation_participant");
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsToMany(User, {through: 'conversation_participant'});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
